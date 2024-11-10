3c0adde6bd5525ae6ae3b000b97d0dd37c923acb
bpopovgit <barbajan@windowslive.com>
2024-11-10 22:58:50 +0200

    feat: added fire rate limit

--- player.py
+++ player.py
@@ -7,6 +7,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shoot_timer = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -23,6 +24,10 @@ class Player(CircleShape):
         self.rotation += PLAYER_TURN_SPEED * dt
 
     def update(self, dt):
+        #Decrease the shoot timer by dt
+        if self.shoot_timer > 0:
+            self.shoot_timer -= dt
+
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -41,7 +46,11 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
 
     def shoot(self):
+        # Only shoot if the shoot_timer is 0 or less
+        if self.shoot_timer <= 0:
+            # Create a new shot at the player's position with velocity in the direction the player is facing
+            velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+            Shot(self.position.x, self.position.y, velocity)
 
-        velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
-
-        shot = Shot(self.position.x, self.position.y, velocity) 
+            # Set the shoot_timer to the cooldown value
+            self.shoot_timer = PLAYER_SHOOT_COOLDOWN
