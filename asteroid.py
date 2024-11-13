80568fa3a0b22e407e31200632b0769731241736
bpopovgit <barbajan@windowslive.com>
2024-11-13 08:58:14 +0200

    added asteroids destruction, splitting large and medium asteroids into smaller ones

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,8 @@
 import pygame
 from circleshape import CircleShape
+from constants import ASTEROID_MIN_RADIUS
+import random
+
 
 class Asteroid(CircleShape):
     containers = None
@@ -19,4 +22,31 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        # Killing the current asteroid
+        self.kill()
+
+        #If the asteroid ist oo small to split, just return
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        # Calculating the radius for the new smaller asteroids
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        
+        # Generating a random angle for splitting between 20 and 50 degrees
+        random_angle = random.uniform(20, 50)
+
+        # Creating two new velocity vectors for the split asteroids
+        new_velocity_1 = self.velocity.rotate(random_angle) * 1.2
+        new_velocity_2 = self.velocity.rotate(-random_angle) * 1.2
+        
+
+        #Creating two new asteroids with the new radius and velocities
+        new_asteroid_1 = Asteroid(self.position.x, self.position.y, new_radius)
+        new_asteroid_1.velocity = new_velocity_1
+
+
+        new_asteroid_2 = Asteroid(self.position.x, self.position.y, new_radius)
+        new_asteroid_2.velocity = new_velocity_2
         
